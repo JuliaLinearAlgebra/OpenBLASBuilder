1a7f3a81cfda573708bc0063b2b199a3b7523e05
Elliot Saba <staticfloat@gmail.com>
2017-11-29 14:40:40 -0800

    Reorganization and fix OSX

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -5,28 +5,44 @@ sources = [
     "5ef38b15d9c652985774869efd548b8e3e972e1e99475c673b25537ed7bcf394",
 ]
 
-common_flags = "USE_THREAD=1 GEMM_MULTITHREADING_THRESHOLD=50 NO_AFFINITY=1 CROSS=1 HOSTCC=\$CC_FOR_BUILD PREFIX=/"
-ilp64_flags  = "INTERFACE64=1 SYMBOLSUFFIX=64_ LIBPREFIX=libopenblas64_"
+# We always want threading
+common_flags  = "USE_THREAD=1 GEMM_MULTITHREADING_THRESHOLD=50 NO_AFFINITY=1 "
+
+# We are cross-compiling
+common_flags *= "CROSS=1 HOSTCC=\$CC_FOR_BUILD PREFIX=/ "
+
+# We need to use our basic objconv, not a prefixed one:
+common_flags *= "OBJCONV=objconv "
+
+# These flags are for if we are building with ILP64 support
+ilp64_flags  = "BINARY=64 INTERFACE64=1 SYMBOLSUFFIX=64_ LIBPREFIX=libopenblas64_"
+
+# From these flags, define the complete flags we'll apply to each platform
 platform_data = Dict(
-    Linux(:x86_64)   => ("DYNAMIC_ARCH=1 BINARY=64 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
+    Linux(:x86_64)   => ("DYNAMIC_ARCH=1 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
     Linux(:i686)     => ("DYNAMIC_ARCH=1 BINARY=32 NUM_THREADS=8", "libopenblasp-r0"),
-    Linux(:aarch64)  => ("TARGET=ARMV8 BINARY=64 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
+    Linux(:aarch64)  => ("TARGET=ARMV8 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
     Linux(:armv7l)   => ("TARGET=ARMV7 BINARY=32 NUM_THREADS=16", "libopenblasp-r0"),
-    Linux(:ppc64le)  => ("TARGET=POWER8 BINARY=64 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
-    MacOS()          => ("DYNAMIC_ARCH=1 BINARY=64 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
-    Windows(:x86_64) => ("DYNAMIC_ARCH=1 BINARY=64 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
+    Linux(:ppc64le)  => ("TARGET=POWER8 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
+    MacOS()          => ("DYNAMIC_ARCH=1 NUM_THREADS=16 $(ilp64_flags) AR=x86_64-apple-darwin14-ar ", "libopenblas64_p-r0"),
+    Windows(:x86_64) => ("DYNAMIC_ARCH=1 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
     Windows(:i686)   => ("DYNAMIC_ARCH=1 BINARY=32 NUM_THREADS=8", "libopenblasp-r0"),
 )
+
+# Add CROSS_SUFFIX to each platform
+platform_data = Dict(k => (v[1] * "CROSS_SUFFIX=$(triplet(k))- ", v[2]) for (k, v) in platform_data)
+
+# Add common_flags to each platform
+platform_data = Dict(k => (v[1] * common_flags, v[2]) for (k, v) in platform_data)
 
 for platform in keys(platform_data)
     platform_flags, libname = platform_data[platform]
 
-    flags = "$(common_flags) $(platform_flags) CROSS_SUFFIX=$(triplet(platform))-"
+    # Construct our script
     script = """
-    cd \${WORKSPACE}/srcdir
-    cd OpenBLAS-0.2.20/
-    make $(flags) -j\${nproc}
-    make $(flags) install
+    cd \${WORKSPACE}/srcdir/OpenBLAS-0.2.20/
+    make $(platform_flags) -j\${nproc}
+    make $(platform_flags) install
     """
 
     products = prefix -> [
