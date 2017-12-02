5497e7de2a10b2421b1fa889957c4b7cd19c8d8f
Elliot Saba <staticfloat@gmail.com>
2017-12-01 21:07:15 -0800

    Fix some spaces

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -24,13 +24,13 @@ platform_data = Dict(
     Linux(:aarch64)  => ("TARGET=ARMV8 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
     Linux(:armv7l)   => ("TARGET=ARMV7 BINARY=32 NUM_THREADS=16", "libopenblasp-r0"),
     Linux(:ppc64le)  => ("TARGET=POWER8 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
-    MacOS()          => ("DYNAMIC_ARCH=1 NUM_THREADS=16 $(ilp64_flags) AR=x86_64-apple-darwin14-ar ", "libopenblas64_p-r0"),
+    MacOS()          => ("DYNAMIC_ARCH=1 NUM_THREADS=16 $(ilp64_flags) AR=x86_64-apple-darwin14-ar", "libopenblas64_p-r0"),
     Windows(:x86_64) => ("DYNAMIC_ARCH=1 NUM_THREADS=16 $(ilp64_flags)", "libopenblas64_p-r0"),
     Windows(:i686)   => ("DYNAMIC_ARCH=1 BINARY=32 NUM_THREADS=8", "libopenblasp-r0"),
 )
 
 # Add CROSS_SUFFIX to each platform
-platform_data = Dict(k => (v[1] * "CROSS_SUFFIX=$(triplet(k))- ", v[2]) for (k, v) in platform_data)
+platform_data = Dict(k => (v[1] * " CROSS_SUFFIX=$(triplet(k))- ", v[2]) for (k, v) in platform_data)
 
 # Add common_flags to each platform
 platform_data = Dict(k => (v[1] * common_flags, v[2]) for (k, v) in platform_data)
