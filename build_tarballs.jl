d3fc7bcbcd0459019d40eb61cc2a24aa32f25cb3
Elliot Saba <staticfloat@gmail.com>
2018-03-06 08:37:33 -0800

    Update to `build_tarballs()`

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -64,14 +64,14 @@ make ${flags} PREFIX=$prefix install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line.
 platforms = [
-    BinaryProvider.Windows(:i686),
-    BinaryProvider.Windows(:x86_64),
-    BinaryProvider.Linux(:i686, :glibc),
-    BinaryProvider.Linux(:x86_64, :glibc),
-    BinaryProvider.Linux(:aarch64, :glibc),
-    BinaryProvider.Linux(:armv7l, :glibc),
-    BinaryProvider.Linux(:powerpc64le, :glibc),
-    BinaryProvider.MacOS()
+    Windows(:i686),
+    Windows(:x86_64),
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc),
+    Linux(:powerpc64le, :glibc),
+    MacOS()
 ]
 
 # The products that we will ensure are always built
@@ -83,45 +83,5 @@ products(prefix) = [
 dependencies = [
 ]
 
-
-# Parse out some command-line arguments
-BUILD_ARGS = ARGS
-
-# This sets whether we should build verbosely or not
-verbose = "--verbose" in BUILD_ARGS
-BUILD_ARGS = filter!(x -> x != "--verbose", BUILD_ARGS)
-
-# This flag skips actually building and instead attempts to reconstruct a
-# build.jl from a GitHub release page.  Use this to automatically deploy a
-# build.jl file even when sharding targets across multiple CI builds.
-only_buildjl = "--only-buildjl" in BUILD_ARGS
-BUILD_ARGS = filter!(x -> x != "--only-buildjl", BUILD_ARGS)
-
-if !only_buildjl
-    # If the user passed in a platform (or a few, comma-separated) on the
-    # command-line, use that instead of our default platforms
-    if length(BUILD_ARGS) > 0
-        platforms = platform_key.(split(BUILD_ARGS[1], ","))
-    end
-    info("Building for $(join(triplet.(platforms), ", "))")
-
-    # Build the given platforms using the given sources
-    autobuild(pwd(), "OpenBLAS", platforms, sources, script, products; dependencies=dependencies, verbose=verbose)
-else
-    # If we're only reconstructing a build.jl file on Travis, grab the information and do it
-    if !haskey(ENV, "TRAVIS_REPO_SLUG") || !haskey(ENV, "TRAVIS_TAG")
-        error("Must provide repository name and tag through Travis-style environment variables!")
-    end
-
-    repo_name = ENV["TRAVIS_REPO_SLUG"]
-    tag_name = ENV["TRAVIS_TAG"]
-    product_hashes = product_hashes_from_github_release(repo_name, tag_name; verbose=verbose)
-    bin_path = "https://github.com/$(repo_name)/releases/download/$(tag_name)"
-    dummy_prefix = Prefix(pwd())
-    print_buildjl(pwd(), products(dummy_prefix), product_hashes, bin_path)
-
-    if verbose
-        info("Writing out the following reconstructed build.jl:")
-        print_buildjl(STDOUT, product_hashes; products=products(dummy_prefix), bin_path)
-    end
-end
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "OpenBLAS", sources, script, platforms, products, dependencies)
