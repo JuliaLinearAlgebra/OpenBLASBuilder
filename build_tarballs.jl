993226fdc9551cf9a99a871d62599bee9033615f
Elliot Saba <staticfloat@gmail.com>
2018-09-21 11:15:33 +0000

    Expand GCC versions

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -100,6 +100,7 @@ done
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line.
 platforms = supported_platforms()
+platforms = expand_gcc_versions(platforms)
 
 # The products that we will ensure are always built
 products(prefix) = [
